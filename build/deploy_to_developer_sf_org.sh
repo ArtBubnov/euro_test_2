533cdfa7f281eb02d9956b9115a6fe8e4f497d62
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-21 11:34:38 +0300

    123

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -4,9 +4,14 @@ echo "Find the difference between organizations"
 PATH_FILTER="./"
 #BASH_VERSION=`bash --ver  sion`
 UPDATED_FILES=$(git diff --name-only origin/dev force-app)
-ONE_MORE_TEST=$(git diff --name-only origin/develop force-app | tr '\n' ',' | sed 's/\(.*\),/\1 /')
+ONE_MORE_TEST=$($UPDATED_FILES | tr '\n' ',')
+
+
+#ONE_MORE_TEST=$(git diff --name-only origin/develop force-app | tr '\n' ',' | sed 's/\(.*\),/\1 /')
+
+
 echo "What is the diff?"
 echo $UPDATED_FILES
 
 echo "One  more test"
-echo $ONE_MORE_TEST
+#echo $ONE_MORE_TEST
