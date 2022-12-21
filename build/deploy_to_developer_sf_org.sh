91ad6ed670d3f8f98ae287d0240ff46368422642
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-21 13:30:49 +0300

    sfdx test

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -7,16 +7,16 @@ UPDATED_FILES=$(git diff --name-only origin/dev force-app | tr '\n' ',' | sed 's
 echo "What is the diff?"
 echo "$UPDATED_FILES"
 
-#echo "Deploy updates to the Salesforce Env"
-#sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_test.org --loglevel WARN
+echo "Deploy updates to the Salesforce Env"
+sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_test.org --loglevel WARN
 
-echo "filesystem test"
+#echo "filesystem test"
 
-echo "PWD test"
-pwd
+#echo "PWD test"
+#pwd
 
-echo "LS test"
-ls
+#echo "LS test"
+#ls
 
-echo "cat test"
-cat force-app/main/default/customMetadata/Trailshred_Setting.Audio_on_Lead_Status_Change.md-meta.xml
+#echo "cat test"
+#cat force-app/main/default/customMetadata/Trailshred_Setting.Audio_on_Lead_Status_Change.md-meta.xml
