32bc6548a5e6368671010e58c699a154d94a9217
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-21 13:04:22 +0300

    sfdx test

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -6,3 +6,6 @@ UPDATED_FILES=$(git diff --name-only origin/dev force-app | tr '\n' ',' | sed 's
 
 echo "What is the diff?"
 echo "$UPDATED_FILES"
+
+echo "Deploy updates to the Salesforce Env"
+sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_test.org --loglevel WARN
