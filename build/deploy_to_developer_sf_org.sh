e422abe232f92d17d1975c9519a3a9788b2d7c72
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-21 17:09:32 +0300

    bad build

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -1,8 +1,10 @@
 echo "Deploy data to Dev Env"
 
-echo "Find the difference between organizations"
-PATH_FILTER="./"
+sfdx force:source:deploy -p force-app -c -l RunLocalTests -u salesforce_build.org --loglevel WARN
+
+#echo "Find the difference between organizations"
+#PATH_FILTER="./"
 #BASH_VERSION=`bash --version`
-UPDATED_FILES=$(git diff --name-only origin/dev force-app)
-echo "What is the diff?"
-echo $UPDATED_FILES
+#UPDATED_FILES=$(git diff --name-only origin/dev force-app)
+#echo "What is the diff?"
+#echo $UPDATED_FILES
