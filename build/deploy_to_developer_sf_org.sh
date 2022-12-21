efd26224e575e0c48500a780ef3785672731355d
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-21 10:44:05 +0300

    deploy_to_dev_org.sh file change

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -3,7 +3,6 @@ echo "Deploy data to Dev Env"
 echo "Find the difference between organizations"
 PATH_FILTER="./"
 #BASH_VERSION=`bash --version`
-UPDATED_FILES=git diff --name-only origin/dev force-app
+UPDATED_FILES=$(git diff --name-only origin/dev force-app)
 echo "What is the diff?"
-echo $UPDATED_FILES
 #echo $BASH_VERSION
