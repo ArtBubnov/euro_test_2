4c8ac80b0335d29bef653ecc128dfdd78edc66f6
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-23 15:33:51 +0300

    tqwea\

--- build/test.sh
+++ build/test.sh
@@ -1,11 +1,18 @@
 echo "test" > test.json
 cat test.json
 
-pwd
 
+echo "before script"
+pwd
+ls
+echo "before script"
 
 curl \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer "$TOKEN\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/ArtBubnov/euro_test_2/pulls > getGithubPRRestApiResponse.json
+
+echo "after script"
+pwd
+ls 
